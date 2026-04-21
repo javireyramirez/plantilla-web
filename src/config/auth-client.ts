515835c42058513a1dc55db93a924e797a71948d
javireyramirez <javireyramirez@gmail.com>
2026-04-22 00:41:08 +0200

    a

--- src/config/auth-client.ts
+++ src/config/auth-client.ts
@@ -1,6 +1,6 @@
 import { createAuthClient } from 'better-auth/react';
 
-const backURL = import.meta.env.DEV ? window.location.origin : 'https://javireyramirez.com';
+const backURL = !import.meta.env.DEV ? window.location.origin : 'https://javireyramirez.com';
 
 export const authClient = createAuthClient({
   baseURL: `${backURL}/api/v1/auth`,
