8db682786fc4d08d85c577bf943028e1fb7f40ac
javireyramirez <javireyramirez@gmail.com>
2026-04-28 00:27:47 +0200

    gap

--- src/modules/home/home.tsx
+++ src/modules/home/home.tsx
@@ -3,7 +3,7 @@ import { FileUploadZone } from '@/components/storage/FileUploadZone';
 
 export default function Home() {
   return (
-    <div className="flex flex-col justify-center items-center">
+    <div className="flex flex-col justify-center items-center gap-6">
       Página genérica de aplicación genérica que es responsive en teléfonosfadhdfh
       <FileUploadZone
         entityType="hola"
